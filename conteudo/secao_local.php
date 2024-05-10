c16d45fb6889fb6800616339c13db4fdcc3a38aa
Diego Monteiro Sousa <36936410+diegomonteirosousa@users.noreply.github.com>
2024-05-10 12:28:58 -0300

    Add files via upload

---
+++ conteudo/secao_local.php
@@ -0,0 +1,25 @@
+<div id="local" class="localizacao pt-3 pb-3">
+	<div class="scroll_animado">
+		<h1 class="txt_capa_4 pb-4">Localização</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<div align="center">
+					<iframe src="https://www.google.com/maps/embed?pb=!4v1640287528122!6m8!1m7!1sZTldxjEYBJubMYltTEh3Rw!2m2!1d-23.64611381447797!2d-46.79579789705938!3f294.30106!4f0!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+					</div>	
+				</div>	
+				<div class="col-sm-6 align-self-center">
+					<div align="center">
+						<h1 class="txt_capa_3">Av. Mississipe</h1>
+						<p class="txt_capa_2">Nº 357 - Jd casa branca</p>
+			            <p class="txt_capa_2">Embu das artes - SP</p>
+			            <span>
+			            	<a href="#ambiente"><button type="button" class="btn btn-outline-light btn-lg texto_botao btn_ambiente btn_laranja">AMBIENTE</button></a>
+			        	</span>
+			       	</div>	
+				</div>	
+
+			</div>
+		</div>
+	</div>                           	
+</div>
---
+++ conteudo/secao_topo.php
@@ -0,0 +1,38 @@
+<!--Mask-->
+  <div id="intro" class="view">
+        <div class="mask rgba-black-strong">
+
+            <div class="container-fluid d-flex align-items-center justify-content-center h-100 centralizador_topo">
+
+                <div class="row d-flex justify-content-center text-center align-self-center ">
+                  <div class="container">
+                      <div class="col-sm-12">
+                          <h1 class="txt_capa_1 sombra">Seu jeito fácil de comer bem!</h1>
+                         
+                          <div class="row">
+                            <div class="col-sm-6 pb-2">
+                              <!-- BOTÃO "PRODUTOS" -->
+                              <span>
+                                <a href="#pratos"><button type="button" class="btn btn-outline-light btn-lg texto_botao">PRATOS</button></a>
+                              </span>
+                            </div>
+                            <div class="col-sm-6 pb-2">
+                              <!-- BOTÃO "PROMOÇÕES" -->
+                              <span>
+                                <a href="#entrega"><button type="button" class="btn btn-outline-light btn-lg texto_botao">ENTREGA</button></a>
+                              </span>
+                            </div>   
+                          </div>
+
+                      </div>
+                  </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+  </div>
+  <!--/.Mask-->
+</header>
